09e85d0d9ccd255839daff6d411e9627473bf000
aoiorio <130330538+aoiorio@users.noreply.github.com>
2023-07-27 18:50:11 +0900

    change the path to HomePage

--- capybara_chat/urls.py
+++ capybara_chat/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
-from .views import SignupPage, LobbyPage
+from .views import SignupPage, HomePage
 
 urlpatterns = [
-    path('', LobbyPage.as_view(), name='lobby'), # viewsファイルで追加したlobbyのパスをurlの後ろに何もなかったらlobby.htmlを返す
+    path('', HomePage.as_view(), name='home'), # viewsファイルで追加したlobbyのパスをurlの後ろに何もなかったらlobby.htmlを返す
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('signup/', SignupPage.as_view(), name='signup')
