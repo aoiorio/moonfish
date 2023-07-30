5a4c6fec9346d50faef60197f62c28f21814553c
aoiorio <130330538+aoiorio@users.noreply.github.com>
2023-07-30 18:50:33 +0900

    added app_name

--- capybara_chat/urls.py
+++ capybara_chat/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
-from .views import SignupPage, HomePage
+from .views import SignupPage
 
+app_name = "capybara_chat"
 urlpatterns = [
-    path('', HomePage.as_view(), name='home'), # viewsファイルで追加したlobbyのパスをurlの後ろに何もなかったらlobby.htmlを返す
+    path('', views.home, name='home'), # viewsファイルで追加したlobbyのパスをurlの後ろに何もなかったらlobby.htmlを返す
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('signup/', SignupPage.as_view(), name='signup')
+    path('signup/', SignupPage.as_view(), name='signup'),
+    path('results/', views.results, name="results")
 ]
