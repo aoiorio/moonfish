8f160edee7ff7f794dbde849ddfe3781b234363b
aoiorio <130330538+aoiorio@users.noreply.github.com>
2023-07-31 14:54:12 +0900

    deleted get_detail function

--- capybara_chat/views.py
+++ capybara_chat/views.py
@@ -10,7 +10,7 @@ from .mixins import (
         FormErrors,
         RedirectParams,
         APIMixin,
-        get_recipe_detail,
+
         random_recipes
 )
 class SignupPage(generic.CreateView):
@@ -27,44 +27,52 @@ class SignupPage(generic.CreateView):
 #     return render(request, 'chat/lobby.html') #chat/lobby.htmlのパスが追加されている
 @login_required(login_url='capybara_chat:login')
 def home(request):
+    # create recommend_recipes
     # checking if the method is POST
     if request.method == 'POST':
         # getting the recipe name from the form input
         query = request.POST.get("query", None)
         if query:
-            return RedirectParams(url = 'capybara_chat:results', params = {"query": query})
-    recommend_recipes = {
-        "random_recipes": random_recipes(21)
-    }
-    print(random_recipes)
-    return render(request, "home/home.html", recommend_recipes)
-
-
-def results(request):
-        query = request.GET.get("query", None)
-        if query:
             results = APIMixin(query=query).get_data()
-
             if results:
-                recipe_urls = []
-                for item in results:
-                    recipe_urls.append(get_recipe_detail(item['id']))
-
-                for recipe in range(len(recipe_urls)):
-                    recipe_url = "recipe_url" + str(recipe)
-                    for item in results:
-                        item[recipe_url] = recipe_urls[recipe]
-
                 context = {
                     "results": results,
                     "query": query,
-                    "recipe_urls": recipe_urls
                 }
+                return render(request, "home/home.html", context) # POSTされて、resultsの中身があったら、contextでresultsとqueryを返す
+    else:
+        recommend_recipes = {
+            "random_recipes": random_recipes(21) # 21個のランダムなレシピを取得
+        }
+        return render(request, "home/home.html", recommend_recipes) # POSTされなかったら、recommend_recipesを返す
+    return render(request, "home/home.html") # これはエラー解消のために書いたもので特に意味はないと思う（絶対にreturnが必要なため）
+
+# 別画面ではなく、同じ画面にしたためコメントアウトした
+# def results(request):
+#         query = request.GET.get("query", None)
+#         if query:
+#             results = APIMixin(query=query).get_data()
+
+#             if results:
+#                 recipe_urls = []
+#                 for item in results:
+#                     recipe_urls.append(get_recipe_detail(item['id']))
+
+#                 for recipe in range(len(recipe_urls)):
+#                     recipe_url = "recipe_url" + str(recipe)
+#                     for item in results:
+#                         item[recipe_url] = recipe_urls[recipe]
+
+#                 context = {
+#                     "results": results,
+#                     "query": query,
+#                     "recipe_urls": recipe_urls
+#                 }
 
 
-                return render(request, 'home/results.html', context)
+#                 return render(request, 'home/results.html', context)
 
-    # the url for recipe, takes query and API_KEY
-    # converting the request response to json
+#     # the url for recipe, takes query and API_KEY
+#     # converting the request response to json
 
-        return render(request, "home/home.html")
+#         return render(request, "home/home.html")
