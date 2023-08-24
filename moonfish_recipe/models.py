039c0936a08d73ad7c993439fb76d97b3d6140c1
aoiorio <130330538+aoiorio@users.noreply.github.com>
2023-08-24 10:20:33 +0800

    added is_favorite in FavoriteRecipes at models.py

--- moonfish_recipe/models.py
+++ moonfish_recipe/models.py
@@ -7,3 +7,4 @@ class FavoriteRecipes(models.Model):
     title = models.CharField(max_length=200, verbose_name="title")
     image = models.CharField(max_length=200, verbose_name="image")
     recipe_url = models.CharField(max_length=300, verbose_name="recipe_url")
+    is_favorite = models.BooleanField(default=False)
