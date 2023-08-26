893a05b39c74550bacbd31390c638e1bfde9f092
aoiorio <130330538+aoiorio@users.noreply.github.com>
2023-08-26 13:13:58 +0800

    update favorite page's UI and rename Foods of DB to Food

--- moonfish_recipe/mixins.py
+++ moonfish_recipe/mixins.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.shortcuts import redirect
+from .models import Food
 
 from urllib.parse import urlencode
 import requests
@@ -26,11 +27,16 @@ class APIMixin:
         url = f"https://api.spoonacular.com/recipes/complexSearch?query={self.query}&addRecipeInformation=true&number=10&apiKey={settings.API_KEY}"
 
         r = requests.get(url)
+
         if r.status_code == 200:
-            try:
-                return r.json()["recipes"]
-            except KeyError:
-                return r.json()['results']
+            # データベースにレシピを追加
+            for recipe in r.json()["results"]:
+                if Food.objects.filter(title=recipe["title"]).exists(): # 同じtitleの要素がDBに入っていたらpassする（要素を追加しない）
+                    pass
+                else:
+                    foods_obj = Food(title=recipe["title"], image=recipe["image"], recipe_url=recipe["sourceUrl"])
+                    foods_obj.save()
+            return r.json()["results"]
         else:
             return None
 
@@ -46,6 +52,13 @@ class APIMixin:
 def random_recipes(self):
     random_recipes_url = f"https://api.spoonacular.com/recipes/random?number={self}&apiKey={settings.API_KEY}" # ランダムなレシピを取得する
     random_recipes_r = requests.get(random_recipes_url)
-
     if random_recipes_r.status_code == 200:
+        # Foodsというデータベースにspoonacular Apiで取得したデータを格納する
+        for recipe in random_recipes_r.json()["recipes"]:
+            if Food.objects.filter(title=recipe["title"]).exists(): # 同じtitleの要素がDBに入っていたらpassする（要素を追加しない）
+                pass
+            else:
+                foods_obj = Food(title=recipe["title"], image=recipe["image"], recipe_url=recipe["sourceUrl"])
+                foods_obj.save()
+        # Foods.objects.delete().values_list("title").distinct()
         return random_recipes_r.json()["recipes"]
