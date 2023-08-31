11dd5f9557069780830478c4e119d602d401755f
aoiorio <130330538+aoiorio@users.noreply.github.com>
2023-08-31 20:15:56 +0900

    update comment

--- moonfish_recipe/models.py
+++ moonfish_recipe/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User # User DBを追加する
 
 class Food(models.Model):
     title = models.CharField(max_length=100, verbose_name="Recipe_title")
     image = models.CharField(max_length=200, verbose_name="Recipe_image_url")
     recipe_url = models.CharField(max_length=300, verbose_name="Recipe_url")
     # is_favorite = models.BooleanField(default=False, verbose_name="Recipe_is_favorite")
-    favorites = models.ManyToManyField(User, related_name="favorites", unique=False) # ManyToManyFieldでログインしているユーザーと連携する
+    favorites = models.ManyToManyField(User, related_name="favorites", unique=False) # ManyToManyFieldでログインしているユーザーと対応する
 
 
 
