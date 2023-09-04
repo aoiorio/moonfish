1e792d3cbb82fd1c71c6fe635a6dc775e63d1427
aoiorio <130330538+aoiorio@users.noreply.github.com>
2023-09-04 19:00:15 +0900

    deleted unnecessary comment.

--- moonfish_recipe/models.py
+++ moonfish_recipe/models.py
@@ -7,15 +7,3 @@ class Food(models.Model):
     recipe_url = models.CharField(max_length=300, verbose_name="Recipe_url")
     # is_favorite = models.BooleanField(default=False, verbose_name="Recipe_is_favorite")
     favorites = models.ManyToManyField(User, related_name="favorites", unique=False) # ManyToManyFieldでログインしているユーザーと対応する
-
-
-
-    # def favoriteObjects(self):
-    #     favorite_food = Food.objects.filter(is_favorite=1)
-    #     return favorite_food
-# class FavoriteRecipes(models.Model):
-#     user = models.ManyToManyField(User, primary_key=True)
-#     title = models.CharField(max_length=200, verbose_name="title")
-#     image = models.CharField(max_length=200, verbose_name="image")
-#     recipe_url = models.CharField(max_length=300, verbose_name="recipe_url")
-#     is_favorite = models.BooleanField(default=False)
