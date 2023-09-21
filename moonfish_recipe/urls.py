0bfe33f6d3f8affb228b41420c15cf9b375b0b5b
aoiorio <130330538+aoiorio@users.noreply.github.com>
2023-09-22 00:09:59 +0900

    deleted comment out

--- moonfish_recipe/urls.py
+++ moonfish_recipe/urls.py
@@ -6,7 +6,6 @@ from .views import SignupPage
 app_name = "moonfish_recipe"
 urlpatterns = [
     path('', views.home, name='home'), # viewsファイルで追加したlobbyのパスをurlの後ろに何もなかったらlobby.htmlを返す
-    # path('/<int:id>', views.home, name="home_is_favorite"),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('signup/', SignupPage.as_view(), name='signup'),
